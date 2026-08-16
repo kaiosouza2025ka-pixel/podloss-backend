6e24cb87927fbc09bd075078a8681861adf5f01a
Loss <kaiosouza2025ka@gmail.com>
2026-08-16 12:13:48 -0300

    Adiciona CORS

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend online!");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+app.post("/deposito", async (req, res) => {
+  const {
+    valor,
+    produto,
+    sabor,
+    quantidade,
+    identificador,
+    cliente,
+    telefone
+  } = req.body;
+
+  console.log({
+    valor,
+    produto,
+    sabor,
+    quantidade,
+    identificador,
+    cliente,
+    telefone
+  });
+
+  res.json({
+    status: "ok",
+    pix_code: "PIX_AQUI"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
